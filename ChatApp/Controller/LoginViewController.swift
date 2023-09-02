6eb7e9d31dca6f6b90f6e440e64d45c2fa0576b2
tahaturan <tahaaturann16@gmail.com>
2023-09-03 02:35:00 +0300

    Login ekrani olsuturulmaya baslandi ve logo icin extension yazildi

--- ChatApp/Controller/LoginViewController.swift
+++ ChatApp/Controller/LoginViewController.swift
@@ -10,14 +10,78 @@ import UIKit
 class LoginViewController: UIViewController {
     //MARK: - Properties
     
+    // Türkçe: Logo görüntüsünü tutmak için özel bir UIImageView oluşturur.
+    // English: Creates a custom UIImageView to hold the logo image.
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        // Türkçe: Bu satır, görüntüyü çerçeve boyutlarına tam olarak sığdırır, ancak orantısız olarak çerçeve boyutlarına genişletir veya sıkıştırır.
+        // English: This line scales the image to completely cover the frame, stretching or compressing it if necessary, resulting in a possibly distorted or non-proportional image fit to the frame.
+        imageView.contentMode = .scaleToFill
+
+        // Türkçe: Resim doldurma ve kırpma özelliklerini ayarlar.
+        // English: Sets the image content mode to scale to fill and enables clipping.
+        imageView.clipsToBounds = true
+        
+        // Türkçe: logoImageView'a bir mesaj sembolü ekler.
+        // English: Adds a message symbol to logoImageView.
+        imageView.image = UIImage(systemName: "ellipsis.message")
+        
+        // Türkçe: Resmin rengini beyaz olarak ayarlar.
+        // English: Sets the color of the image to white.
+        imageView.tintColor = .white
+        
+        return imageView
+    }()
     
     //MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        // Türkçe: Ana görünüme bir gradyan katmanı ekler.
+        // English: Adds a gradient layer to the main view.
+        addGradientLayer(to: view)
+        
+        // Türkçe: Özel stilleri uygular.
+        // English: Applies custom styles.
+        style()
+        
+        // Türkçe: Arayüz öğelerini düzenler.
+        // English: Arranges interface elements.
+        layout()
     }
+}
 
+//MARK: - Helper
+extension LoginViewController {
+    private func style() {
+        // Türkçe: logoImageView'un otomatik boyutlandırmayı devre dışı bırakır.
+        // English: Disables automatic resizing for logoImageView.
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        // Türkçe: LogoImageView'u ana görünüme ekler.
+        // English: Adds logoImageView to the main view.
+        view.addSubview(logoImageView)
+        
+        NSLayoutConstraint.activate([
+            // Türkçe: LogoImageView'un üst kenarını güvenli alanın üst kenarına 16 birim uzaklıkta konumlandırır.
+            // English: Positions logoImageView 16 units below the top edge of the safe area.
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            
+            // Türkçe: LogoImageView'un yüksekliğini 150 birime sabitler.
+            // English: Sets the height of logoImageView to 150 units.
+            logoImageView.heightAnchor.constraint(equalToConstant: 150),
+            
+            // Türkçe: LogoImageView'un genişliğini 150 birime sabitler.
+            // English: Sets the width of logoImageView to 150 units.
+            logoImageView.widthAnchor.constraint(equalToConstant: 150),
 
+            // Türkçe: LogoImageView'u ana görünümün ortasına hizalar.
+            // English: Centers logoImageView horizontally in the main view.
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
 
---
+++ ChatApp/Utils/Extension.swift
@@ -0,0 +1,35 @@
+//
+//  Extension.swift
+//  ChatApp
+//
+//  Created by Taha Turan on 2.09.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    ///Adds a gradient layer to a view. // Görünüme bir gradient katmanı ekler.
+    ///
+    /// - Parameters:
+    ///   - view: The view to which the gradient layer will be added. // Gradient katmanının ekleneceği görünüm.
+    ///   - colors: The list of colors to be used in the gradient. // Gradientte kullanılacak renklerin listesi.
+    ///   - locations: The distribution of colors in the gradient. // Gradientin renklerinin dağılımı.
+    func addGradientLayer(to view: UIView, colors: [UIColor] = [.systemBlue, .systemPink, .white], locations: [NSNumber] = [0, 0.5, 1]) {
+        let gradient = CAGradientLayer()
+        gradient.locations = locations
+        
+        // Gradient renklerini belirlemek için UIColor dizisini CGColor dizisine dönüştürür.
+        // This line converts the UIColor array to a CGColor array to specify gradient colors.
+        gradient.colors = colors.map { $0.cgColor }
+
+        // Gradient katmanının boyutunu, görüntülenen UIView nesnesinin boyutuna ayarlar.
+        // The size of the gradient layer is set to match the dimensions of the displayed UIView.
+        gradient.frame = view.bounds
+
+        // Gradient katmanını UIView nesnesinin altına ekler. 0, katmanın en altta olacağı anlamına gelir.
+        // This line adds the gradient layer beneath the UIView, with 0 indicating it will be at the very bottom.
+        view.layer.insertSublayer(gradient, at: 0)
+
+    }
+
+}
