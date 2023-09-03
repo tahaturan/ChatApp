b9c3f215198edcfb52d6f604f4dbb12f64e7ae00
tahaturan <tahaaturann16@gmail.com>
2023-09-03 17:29:46 +0300

    Color sabitlerinin olsulturulmasi

--- ChatApp/Utils/AppConstants.swift
+++ ChatApp/Utils/AppConstants.swift
@@ -5,7 +5,7 @@
 //  Created by Taha Turan on 3.09.2023.
 //
 
-import Foundation
+import UIKit
 
 
 struct K {
@@ -20,6 +20,15 @@ struct K {
     struct Icons {
         static let logoImage: String = "ellipsis.message"
         static let mailIcon: String = "envelope"
+        static let lockIcon: String = "lock"
+    }
+    
+    struct Colors {
+        static let darkDenimBlue: UIColor = UIColor(red: 5/255, green: 59/255, blue: 80/255, alpha: 1)
+        static let bondi: UIColor = UIColor(red: 23/255, green: 107/255, blue: 135/255, alpha: 1)
+        static let nileStone: UIColor = UIColor(red: 100/255, green: 204/255, blue: 197/255, alpha: 1)
+        static let superSilver: UIColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+        static let colorgradiendList: [UIColor] = [darkDenimBlue, bondi, nileStone, superSilver]
     }
     
 }
