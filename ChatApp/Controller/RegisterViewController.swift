dd34a132909b5a2a32be019ad2ce3b5bbcbdbfba
tahaturan <tahaaturann16@gmail.com>
2023-09-06 00:28:33 +0300

    RegisterViewController olusutuldu

---
+++ ChatApp/Controller/RegisterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterViewController.swift
+//  ChatApp
+//
+//  Created by Taha Turan on 6.09.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    //MARK: - Properties
+    //MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+    
+}
+
+//MARK: - Helper
+
+extension RegisterViewController {
+    //Setup Style
+    private func style() {
+        view.backgroundColor = .red
+    }
+    // Setup Layout
+    private func layout() {
+        
+    }
+}
