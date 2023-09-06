d1608a5a6cdc3c79f16613f4d758b1792810976a
tahaturan <tahaaturann16@gmail.com>
2023-09-07 01:53:03 +0300

    Firebase projeye dahil edildi

--- ChatApp/Files/AppDelegate.swift
+++ ChatApp/Files/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow()
         window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
         window?.makeKeyAndVisible()
+        FirebaseApp.configure()
         return true
     }
 
