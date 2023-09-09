5ce46cbc66281f21f804b178221caf3e029cbb35
tahaturan <tahaaturann16@gmail.com>
2023-09-09 03:59:27 +0300

    App Error dosyasi olsuturuldu hata yonetimi icin

---
+++ ChatApp/Model/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  ChatApp
+//
+//  Created by Taha Turan on 9.09.2023.
+//
+
+import Foundation
---
+++ ChatApp/Utils/AppError.swift
@@ -0,0 +1,31 @@
+//
+//  AppError.swift
+//  ChatApp
+//
+//  Created by Taha Turan on 9.09.2023.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case profileImageCreationError
+    case imageUploadError
+    case downloadUrlError
+    case userCreationError
+    case firestoreError
+
+    var localizedDescription: String {
+        switch self {
+        case .profileImageCreationError:
+            return K.AppErrorLocalizedDescription.profileImageCreationError
+        case .imageUploadError:
+            return K.AppErrorLocalizedDescription.imageUploadError
+        case .downloadUrlError:
+            return K.AppErrorLocalizedDescription.downloadUrlError
+        case .userCreationError:
+            return K.AppErrorLocalizedDescription.userCreationError
+        case .firestoreError:
+            return K.AppErrorLocalizedDescription.firestoreError
+        }
+    }
+}
