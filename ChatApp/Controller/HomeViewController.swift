8a8d33ac465fb59925036c45a039c45459158880
tahaturan <tahaaturann16@gmail.com>
2023-09-10 09:52:58 +0300

    UINavigationApperance eklenmesi

--- ChatApp/Controller/HomeViewController.swift
+++ ChatApp/Controller/HomeViewController.swift
@@ -16,7 +16,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      signOut()
+      authenticationStatus()
         style()
         layout()
     }
--- ChatApp/Files/AppDelegate.swift
+++ ChatApp/Files/AppDelegate.swift
@@ -5,20 +5,38 @@
 //  Created by Taha Turan on 2.09.2023.
 //
 
-import UIKit
 import FirebaseCore
+import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window?.rootViewController = createStyledNavigationController(rootViewController: HomeViewController())
         window?.makeKeyAndVisible()
         FirebaseApp.configure()
         return true
     }
 
-}
+    // Bu fonksiyon, özelleştirilmiş bir gezinme denetleyici oluşturur.
+    // This function creates a customized navigation controller.
+    private func createStyledNavigationController(rootViewController: UIViewController, barTintColor: UIColor = .white) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
 
+        // Gezinme çubuğu görünümünü yapılandır
+        // Configure the navigation bar appearance
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = barTintColor
+
+        // Gezinme çubuğu görünümlerini ayarla
+        // Set navigation bar appearances
+        navigationController.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController.navigationBar.compactAppearance = navigationBarAppearance
+        navigationController.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        navigationController.navigationBar.compactScrollEdgeAppearance = navigationBarAppearance
+
+        return navigationController
+    }
+}
